06a2a86408ba57d51278a9d6ac19137edda14e5b
Mahmuttok1234 <89297825+Mahmuttok1234@users.noreply.github.com>
2022-03-16 09:16:45 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -11,8 +11,8 @@ let handler = async(m, { conn }) => {
 Silahkan klik pada "List Harga" untuk melihat list.
 
 Pembayaran:
-Pulsa : 085730903853
-GoPay: 085730903853
+Pulsa : 081217445262
+GoPay: 081217445262
 `.trim()
     const button = {
         buttonText: 'List Harga',
